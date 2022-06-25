81d7feb948eb669060be36264c590ae20d3df51a
oladips <oladips200@gmail.com>
2022-06-25 17:15:07 +0000

    first commit

---
+++ schemas/property.js
@@ -0,0 +1,18 @@
+/** @format */
+export default {
+  name: "property",
+  title: "Property",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "location",
+      title: "Location",
+      type: "string",
+    },
+  ],
+};
---
+++ schemas/schema.js
@@ -0,0 +1,16 @@
+/** @format */
+import createSchema from "part:@sanity/base/schema-creator";
+import schemaTypes from "all:part:@sanity/base/schema-type";
+import property from "./property";
+
+// Then we give our schema to the builder and provide the result to Sanity
+export default createSchema({
+  // We name our schema
+  name: "default",
+  // Then proceed to concatenate our document type
+  // to the ones provided by any plugins that are installed
+  types: schemaTypes.concat([
+    property,
+    /* Your types here! */
+  ]),
+});
