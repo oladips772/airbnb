0163165718c3067bb824eca73521f3bf993236e1
oladips <oladips200@gmail.com>
2022-06-26 08:21:25 +0000

    creating property schema

--- schemas/property.js
+++ schemas/property.js
@@ -12,7 +12,81 @@ export default {
     {
       name: "location",
       title: "Location",
+      type: "geopoint",
+    },
+    {
+      name: "images",
+      title: "Images",
+      type: "array",
+      of: [{ type: "propertyImage" }],
+    },
+    {
+      name: "mainImage",
+      title: "Main Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "pricePerNight",
+      title: "Price Per Night",
+      type: "number",
+    },
+    {
+      name: "beds",
+      title: "Beds",
+      type: "number",
+    },
+    {
+      name: "bedrooms",
+      title: "Bedrooms",
+      type: "number",
+    },
+    {
+      name: "propertyType",
+      title: "Property Type",
       type: "string",
+      options: {
+        list: [
+          { title: "Apartment", value: "Apartment" },
+          { title: "House", value: "House" },
+          { title: "Townhouse", value: "Townhouse" },
+          { title: "Duplex", value: "Duplex" },
+          { title: "Condo", value: "Condo" },
+        ],
+        layout: "radio",
+      },
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 100,
+      },
+    },
+    {
+      name: "host",
+      title: "host",
+      type: "host",
+    },
+    {
+      name: "reviews",
+      title: "Reviews",
+      type: "array",
+      of: [{ type: "review" }],
+    },
+    {
+      name: "id",
+      title: "ID",
+      type: "number",
     },
   ],
 };
---
+++ schemas/propertyImage.js
@@ -0,0 +1,19 @@
+/** @format */
+export default {
+  name: "propertyImage",
+  title: "Property Image",
+  type: "image",
+  fields: [
+    {
+      name: "caption",
+      title: "Caption",
+      type: "string",
+      options: {
+        isHighlight: true,
+      },
+    },
+  ],
+  options: {
+    hotspot: true,
+  },
+};
